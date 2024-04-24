6c843faf6307f6ff18e31293e00e28fb4e8b7a33
versecafe <147033096+versecafe@users.noreply.github.com>
2024-04-23 22:34:35 -0400

    hide ALL sensitive data from analytics

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -1,9 +1,11 @@
 /** Remove sensitive data from URLs used in analytics & speed insights */
 export function cleanSensitiveData(url: string): string {
-  // Remove the token from the URL and replace it with [token] & remove the query params related to the token
+  // RRemove all semi sensitive data from the URL
   if (url.includes("/scrape")) {
-    // filter out the token from the URL path and replace it with [token]
-    url = url.replace(/(\/scrape\/)[^\/]+(\/[^\/]+\/[^\/]+)/, "$1[token]$2");
+    url = url.replace(
+      /scrape\/[^\/]+\/[^\/]+\/[^\/]+/,
+      "scrape/[token]/[teamId]/[projectId]",
+    );
     // remove any query params related to the token
     const temp = new URL(url);
     temp.searchParams.delete("token");
